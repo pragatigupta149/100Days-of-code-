a553254cb0303d3427987b86447b76b18af82481
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-10-30 16:29:47 +0530

    Add files via upload

---
+++ day2code1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>   // Header file for input and output functions
+
+int main() {
+    float length, breadth;        // Variables to store dimensions
+    float area, perimeter;        // Variables to store results
+
+    // Taking input from the user
+    printf("Enter the length of the rectangle: ");
+    scanf("%f", &length);
+
+    printf("Enter the breadth of the rectangle: ");
+    scanf("%f", &breadth);
+
+    // Calculating area and perimeter
+    area = length * breadth;
+    perimeter = 2 * (length + breadth);
+
+    // Displaying the results
+    printf("\nArea of the rectangle = %.2f\n", area);
+    printf("Perimeter of the rectangle = %.2f\n", perimeter);
+
+    return 0;   // Program executed successfully
+}
