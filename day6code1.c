aa4f7a1cb8da2034e79cb1e6c2ffb335b7dd1919
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-10-31 16:28:28 +0530

    Add files via upload

---
+++ day6code1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>   // Header file for input and output functions
+
+int main() {
+    int num;   // Variable to store the input number
+
+    // Taking input from the user
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    // Checking whether the number is even or odd
+    if (num % 2 == 0) {
+        printf("%d is an even number.\n", num);
+    } else {
+        printf("%d is an odd number.\n", num);
+    }
+
+    return 0;   // Program executed successfully
+}
