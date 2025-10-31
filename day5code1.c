a6dd98279076ef0ce8728ba17006fb969ba61209
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-10-31 16:24:42 +0530

    Add files via upload

---
+++ day5code1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>    // For input and output functions
+#include <math.h>     // For pow() function used in compound interest
+
+int main() {
+    float principal, rate, time;               // Input variables
+    float simple_interest, compound_interest;  // Result variables
+
+    // Taking input from the user
+    printf("Enter Principal amount: ");
+    scanf("%f", &principal);
+
+    printf("Enter Rate of Interest (in %%): ");
+    scanf("%f", &rate);
+
+    printf("Enter Time (in years): ");
+    scanf("%f", &time);
+
+    // Calculating Simple Interest
+    simple_interest = (principal * rate * time) / 100;
+
+    // Calculating Compound Interest
+    compound_interest = principal * (pow((1 + rate / 100), time)) - principal;
+
+    // Displaying the results
+    printf("\nSimple Interest = %.2f\n", simple_interest);
+    printf("Compound Interest = %.2f\n", compound_interest);
+
+    return 0;   // Program executed successfully
+}
