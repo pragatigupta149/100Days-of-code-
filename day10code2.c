9a42ad87c70725d84293a3663985105a6c49d5ec
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-10-31 16:54:57 +0530

    Add files via upload

---
+++ day10code2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>   // Header file for input and output functions
+
+int main() {
+    int day;   // Variable to store day number
+
+    // Taking input from the user
+    printf("Enter a number (1-7): ");
+    scanf("%d", &day);
+
+    // Displaying the corresponding day using switch-case
+    switch (day) {
+        case 1:
+            printf("Monday\n");
+            break;
+        case 2:
+            printf("Tuesday\n");
+            break;
+        case 3:
+            printf("Wednesday\n");
+            break;
+        case 4:
+            printf("Thursday\n");
+            break;
+        case 5:
+            printf("Friday\n");
+            break;
+        case 6:
+            printf("Saturday\n");
+            break;
+        case 7:
+            printf("Sunday\n");
+            break;
+        default:
+            printf("Invalid input! Please enter a number between 1 and 7.\n");
+            break;
+    }
+
+    return 0;   // Program executed successfully
+}
