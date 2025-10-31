b9a45617774594f5f4d3afe52b4a3d32d43e6f1a
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-10-31 16:36:01 +0530

    Add files via upload

---
+++ day8code2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>   // Header file for input and output functions
+
+int main() {
+    int num1, num2, num3;   // Variables to store three numbers
+
+    // Taking input from the user
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+
+    // Checking which number is the largest using if–else
+    if (num1 >= num2 && num1 >= num3) {
+        printf("%d is the largest number.\n", num1);
+    }
+    else if (num2 >= num1 && num2 >= num3) {
+        printf("%d is the largest number.\n", num2);
+    }
+    else {
+        printf("%d is the largest number.\n", num3);
+    }
+
+    return 0;   // Program executed successfully
+}
