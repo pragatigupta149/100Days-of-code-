60d1034161e7bb074db829d5479a4c27c2690283
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-10-31 16:43:10 +0530

    Add files via upload

---
+++ day10code1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>   // Header file for input and output functions
+
+int main() {
+    float side1, side2, side3;   // Variables to store side lengths
+
+    // Taking input from the user
+    printf("Enter the three sides of the triangle: ");
+    scanf("%f %f %f", &side1, &side2, &side3);
+
+    // Check if sides form a valid triangle
+    if ((side1 + side2 > side3) && 
+        (side2 + side3 > side1) && 
+        (side1 + side3 > side2)) {
+
+        // Classify the triangle
+        if (side1 == side2 && side2 == side3) {
+            printf("The triangle is Equilateral.\n");
+        }
+        else if (side1 == side2 || side2 == side3 || side1 == side3) {
+            printf("The triangle is Isosceles.\n");
+        }
+        else {
+            printf("The triangle is Scalene.\n");
+        }
+    }
+    else {
+        printf("The given sides do not form a valid triangle.\n");
+    }
+
+    return 0;   // Program executed successfully
+}
