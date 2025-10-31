c801220c230a39f68938b6a3e4356eb1809553f4
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-10-31 16:33:51 +0530

    Add files via upload

---
+++ day8code1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>   // Header file for input and output functions
+
+int main() {
+    char ch;   // Variable to store the character
+
+    // Taking input from the user
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    // Checking character type using if–else ladder
+    if (ch >= 'A' && ch <= 'Z') {
+        printf("%c is an uppercase alphabet.\n", ch);
+    }
+    else if (ch >= 'a' && ch <= 'z') {
+        printf("%c is a lowercase alphabet.\n", ch);
+    }
+    else if (ch >= '0' && ch <= '9') {
+        printf("%c is a digit.\n", ch);
+    }
+    else {
+        printf("%c is a special character.\n", ch);
+    }
+
+    return 0;   // Program executed successfully
+}
