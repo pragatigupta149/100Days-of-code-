41db00afe51fb520aec3473cd12069cdc400c8e1
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-01 20:35:25 +0530

    Add files via upload

---
+++ day18code1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>   // Header file for input and output functions
+
+int main() {
+    int num, i;
+
+    // Taking input from the user
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    // Check for valid input
+    if (num <= 0) {
+        printf("Please enter a positive integer.\n");
+        return 0;
+    }
+
+    printf("Factors of %d are: ", num);
+
+    // Loop to find all factors
+    for (i = 1; i <= num; i++) {
+        if (num % i == 0) {     // If divisible, it's a factor
+            printf("%d ", i);
+        }
+    }
+
+    printf("\n");  // Move to the next line after printing all factors
+
+    return 0;   // Program executed successfully
+}
