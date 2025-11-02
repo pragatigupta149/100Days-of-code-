9d09bd898b80d32f3cdf7f45c21c6563b09fa05f
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-02 14:34:29 +0530

    Add files via upload

---
+++ 27code2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, space;
+
+    // Upper half of the diamond
+    for (i = 1; i <= 4; i++) {
+        // Print leading spaces
+        for (space = i; space < 4; space++) {
+            printf(" ");
+        }
+
+        // Print stars
+        for (j = 1; j <= (2 * i - 1); j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    // Lower half of the diamond
+    for (i = 3; i >= 1; i--) {
+        // Print leading spaces
+        for (space = 3; space >= i; space--) {
+            printf(" ");
+        }
+
+        // Print stars
+        for (j = 1; j <= (2 * i - 1); j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
