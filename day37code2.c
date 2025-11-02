6d16860909d86a18ddbeb4bffa24ea06d894038f
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-02 15:35:23 +0530

    Add files via upload

---
+++ day37code2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols, i, j;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols], transpose[cols][rows];
+
+    // Input matrix elements
+    printf("Enter the elements of the matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Calculate transpose
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+
+    // Display original matrix
+    printf("\nOriginal Matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Display transpose matrix
+    printf("\nTranspose of the Matrix:\n");
+    for (i = 0; i < cols; i++) {
+        for (j = 0; j < rows; j++) {
+            printf("%d ", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
