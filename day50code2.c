8a94970e47f0ca0a971adc171526e1d1313e2dd9
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-02 16:08:35 +0530

    Add files via upload

---
+++ day50code2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int i, j, k;
+
+    printf("Enter a string: ");
+    gets(str);  // read the string
+
+    int len = strlen(str);
+
+    printf("\nAll possible substrings are:\n");
+
+    // Generate all substrings
+    for (i = 0; i < len; i++) {
+        for (j = i; j < len; j++) {
+            // Print substring from i to j
+            for (k = i; k <= j; k++) {
+                printf("%c", str[k]);
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
