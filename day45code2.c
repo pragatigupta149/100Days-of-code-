ca9051bcf1fd618d71bf6bbe7790749d244a5f0d
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-02 15:57:19 +0530

    Add files via upload

---
+++ day45code2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    int i;
+
+    printf("Enter a string: ");
+    gets(str);  // Input string
+
+    // Toggle each character
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z')
+            str[i] = str[i] - 32;  // Convert lowercase to uppercase
+        else if (str[i] >= 'A' && str[i] <= 'Z')
+            str[i] = str[i] + 32;  // Convert uppercase to lowercase
+    }
+
+    printf("\nToggled string: %s\n", str);
+
+    return 0;
+}
