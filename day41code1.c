6d80f8752e90eff0fda3c12ebdc750d82ecc1a25
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-02 15:47:08 +0530

    Add files via upload

---
+++ day41code1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, count = 0;
+
+    printf("Enter a string: ");
+    gets(str);  // Taking string input
+
+    // Count characters manually
+    for (i = 0; str[i] != '\0'; i++) {
+        count++;
+    }
+
+    printf("Number of characters in the string: %d\n", count);
+
+    return 0;
+}
