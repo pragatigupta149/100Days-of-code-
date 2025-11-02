d585eb472be593242f61bf63eef3f92048590372
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-02 16:02:17 +0530

    Add files via upload

---
+++ day48code1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100], temp[200];
+
+    printf("Enter first string: ");
+    gets(str1);
+
+    printf("Enter second string: ");
+    gets(str2);
+
+    // Check if lengths are equal
+    if (strlen(str1) != strlen(str2)) {
+        printf("\nStrings are NOT rotations of each other.\n");
+        return 0;
+    }
+
+    // Concatenate str1 with itself
+    strcpy(temp, str1);
+    strcat(temp, str1);
+
+    // Check if str2 is a substring of temp
+    if (strstr(temp, str2) != NULL)
+        printf("\nStrings ARE rotations of each other.\n");
+    else
+        printf("\nStrings are NOT rotations of each other.\n");
+
+    return 0;
+}
