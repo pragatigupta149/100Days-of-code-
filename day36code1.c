8d3f9805681718aad8168b0ca8dd7f6603023a1a
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-02 15:30:48 +0530

    Add files via upload

---
+++ day36code1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols, i, j;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols];
+
+    // Input elements
+    printf("Enter the elements of the matrix:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Display matrix
+    printf("\nThe matrix is:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
