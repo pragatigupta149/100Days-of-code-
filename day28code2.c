628c52cab34a24e4b6107a847b3fad31c8f0ce55
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-02 14:39:06 +0530

    Add files via upload

---
+++ day28code2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n]; // Declare an array of size n
+
+    // Input elements
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Print elements
+    printf("The elements of the array are:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+    return 0;
+}
