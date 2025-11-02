a043b18267117ab46eb80b2efa641e663b966ff4
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-02 15:48:10 +0530

    Add files via upload

---
+++ day41code2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i;
+
+    printf("Enter a string: ");
+    gets(str);  // Input the string (can include spaces)
+
+    printf("\nCharacters of the string are:\n");
+
+    // Loop through each character until '\0'
+    for (i = 0; str[i] != '\0'; i++) {
+        printf("%c\n", str[i]);
+    }
+
+    return 0;
+}
