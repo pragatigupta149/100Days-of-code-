5eb9580ea475a9569ab514905838e15dcb85ec05
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-02 16:44:01 +0530

    Add files via upload

---
+++ day66code1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n, target, i, j, found = 0;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int nums[n];
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &nums[i]);
+
+    printf("Enter target: ");
+    scanf("%d", &target);
+
+    for (i = 0; i < n - 1; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (nums[i] + nums[j] == target) {
+                printf("%d %d\n", i, j);
+                found = 1;
+                break;
+            }
+        }
+        if (found) break;
+    }
+
+    if (!found)
+        printf("-1 -1\n");
+
+    return 0;
+}
