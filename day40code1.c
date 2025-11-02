be1c6e0c0f0858de978426e6558e664ad1d526cd
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-02 15:44:04 +0530

    Add files via upload

---
+++ day40code1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+
+    printf("Enter the size of the square matrix (n x n): ");
+    scanf("%d", &n);
+
+    int matrix[n][n];
+
+    // Input matrix elements
+    printf("Enter elements of the matrix:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Display matrix
+    printf("\nThe matrix is:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Diagonal traversal
+    printf("\nDiagonal traversal (main diagonal elements):\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", matrix[i][i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
