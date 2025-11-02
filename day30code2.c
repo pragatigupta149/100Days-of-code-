6eaf604d8015de8a2ea3ad8ac499d4b40a336412
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-02 14:52:08 +0530

    Add files via upload

---
+++ day30code2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int positiveCount = 0, negativeCount = 0, zeroCount = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n]; // Declare array of size n
+
+    // Input elements
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Count positive, negative, and zero elements
+    for (i = 0; i < n; i++) {
+        if (arr[i] > 0)
+            positiveCount++;
+        else if (arr[i] < 0)
+            negativeCount++;
+        else
+            zeroCount++;
+    }
+
+    // Display results
+    printf("Positive elements = %d\n", positiveCount);
+    printf("Negative elements = %d\n", negativeCount);
+    printf("Zero elements = %d\n", zeroCount);
+
+    return 0;
+}
