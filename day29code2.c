9a074e5d72a63a51313962e81624d2c9c3fc1a09
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-02 14:41:39 +0530

    Add files via upload

---
+++ day29code2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, max, min;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n]; // Declare array of size n
+
+    // Input elements
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Initialize max and min with the first element
+    max = arr[0];
+    min = arr[0];
+
+    // Find max and min
+    for (i = 1; i < n; i++) {
+        if (arr[i] > max)
+            max = arr[i];
+        if (arr[i] < min)
+            min = arr[i];
+    }
+
+    // Display results
+    printf("Maximum element = %d\n", max);
+    printf("Minimum element = %d\n", min);
+
+    return 0;
+}
