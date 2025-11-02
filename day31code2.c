412ee2dfd0f5a1656456735895c8a0f3d8dec19a
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-02 14:55:57 +0530

    Add files via upload

---
+++ day31code2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, temp;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n]; // Declare array of size n
+
+    // Input elements
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Reverse the array in place
+    for (i = 0; i < n / 2; i++) {
+        temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
+    }
+
+    // Display the reversed array
+    printf("Reversed array:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+    return 0;
+}
