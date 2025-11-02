4698230cea5b058824172ab989c29f7e1c757fe5
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-02 14:21:56 +0530

    Add files via upload

---
+++ day25code1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int i, j;
+
+    // Outer loop controls number of rows
+    for (i = 5; i >= 1; i--) {
+        // Inner loop prints numbers from i to 5
+        for (j = i; j <= 5; j++) {
+            printf("%d", j);
+        }
+        printf("\n"); // Move to next line after each row
+    }
+
+    return 0;
+}
