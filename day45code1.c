e087ca1df0d7fb709dbc8f21991118bd1654c272
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-02 15:56:18 +0530

    Add files via upload

---
+++ day45code1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char str[200], ch;
+    int i, count = 0;
+
+    printf("Enter a string: ");
+    gets(str);  // Input string
+
+    printf("Enter a character to find its frequency: ");
+    scanf("%c", &ch);  // Input character
+
+    // Count frequency of the given character
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ch)
+            count++;
+    }
+
+    printf("\nThe character '%c' appears %d times in the string.\n", ch, count);
+
+    return 0;
+}
