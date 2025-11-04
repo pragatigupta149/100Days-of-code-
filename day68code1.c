e504a3849764146731df93f0967b43d7003efc17
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-04 14:48:32 +0530

    Update day68code1.c

--- day68code1.c
+++ day68code1.c
@@ -1,3 +1,5 @@
+//Q118. Write a program to take an input array of size n. The array should contain all the integers between 0 to n except for one. Print that missing number
+
 #include <stdio.h>
 
 int main() {
