996291ab92371660df1315957b5ceb8d5391f442
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-04 14:46:36 +0530

    Update day66code1.c

--- day66code1.c
+++ day66code1.c
@@ -1,3 +1,5 @@
+//Q116. Write a program to take an integer array nums which contains only positive integers, and an integer target as inputs. The goal is to find two distinct indices i and j in the array such that nums[i] + nums[j] equals the target. Assume exactly one solution exists and return the indices in any order. Print the two indices separated by a space as output. If no solution exists, print "-1 -1".
+
 #include <stdio.h>
 
 int main() {
