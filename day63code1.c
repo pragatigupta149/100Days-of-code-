18a8ae3f52209f7045660db18c9657c20ba0b257
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-04 14:42:53 +0530

    Update day63code1.c

--- day63code1.c
+++ day63code1.c
@@ -1,3 +1,6 @@
+//Q113. Write a program to take an integer array arr and an integer k as inputs. The task is to find the kth smallest element in the array. Print the kth smallest element as output.
+
+
 #include <stdio.h>
 
 int main() {
