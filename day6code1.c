48e59423f34e49f314f9e16b3af84b68d01a81ca
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-04 14:59:40 +0530

    Update day6code1.c

--- day6code1.c
+++ day6code1.c
@@ -1,5 +1,6 @@
-Write a program to input an integer and check whether it is even or odd using if–else.
-    #include <stdio.h>   // Header file for input and output functions
+//Write a program to input an integer and check whether it is even or odd using if–else.
+   
+#include <stdio.h>   // Header file for input and output functions
 
 int main() {
     int num;   // Variable to store the input number
