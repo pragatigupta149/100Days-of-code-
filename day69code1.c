a8613d1124637ec7f0516b8efba902cfdd7e1929
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-04 14:49:57 +0530

    Update day69code1.c

--- day69code1.c
+++ day69code1.c
@@ -1,3 +1,5 @@
+//Q119. Write a program to take an integer array as input. Only one element will be repeated. Print the repeated element. Try to find the result in one single iteration.
+
 #include <stdio.h>
 
 int main() {
