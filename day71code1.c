e1cc3b4647a7143a275b89ca727d472d399cba97
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-28 21:48:11 +0530

    Update day71code1.c

--- day71code1.c
+++ day71code1.c
@@ -1,4 +1,7 @@
-#include <stdio.h>
+//Write a C program that creates a text file named info.txt in write mode. The program should take the user’s name and age as input, and write them to the file using fprintf(). After writing, display a message confirming that the data was successfully saved.
+    
+    
+    #include <stdio.h>
 
 int main() {
     FILE *fp;
