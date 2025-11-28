8a1daf9dec7ef100104f84ba794ccfcba82c3853
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-28 22:15:28 +0530

    Update day80code1.c

--- day80code1.c
+++ day80code1.c
@@ -1,3 +1,5 @@
+//Store multiple student records (name, roll number, marks) into a file using fprintf(). Then read them using fscanf() and display each record.
+
 #include <stdio.h>
 
 struct Student {
