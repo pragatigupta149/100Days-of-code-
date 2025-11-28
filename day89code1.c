a796e0b5ff4733fd98960bca1f2525f9bcb329ab
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-28 23:06:14 +0530

    Add files via upload

---
+++ day89code1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+enum Colors {
+    RED = 1,
+    BLUE,
+    GREEN,
+    YELLOW,
+    BLACK
+};
+
+int main() {
+    enum Colors c;
+
+    printf("Enum names and their integer values:\n\n");
+
+    for (c = RED; c <= BLACK; c++) {
+        switch (c) {
+            case RED:
+                printf("RED = %d\n", c);
+                break;
+            case BLUE:
+                printf("BLUE = %d\n", c);
+                break;
+            case GREEN:
+                printf("GREEN = %d\n", c);
+                break;
+            case YELLOW:
+                printf("YELLOW = %d\n", c);
+                break;
+            case BLACK:
+                printf("BLACK = %d\n", c);
+                break;
+        }
+    }
+
+    return 0;
+}
