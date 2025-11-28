71ca682c208ecc36638b8bb38cf59bed5791ce50
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-28 21:51:35 +0530

    Update day72code1.c

--- day72code1.c
+++ day72code1.c
@@ -1,3 +1,6 @@
+//Write a C program that opens an existing file (e.g., info.txt) and reads its contents using fgets(). The program should print all the lines to the console until EOF (end of file) is reached.
+
+
 #include <stdio.h>
 
 int main() {
