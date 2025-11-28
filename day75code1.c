209ba57edd8bd6f6c5c5842fdde2043635ff4ec8
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-28 21:58:57 +0530

    Update day75code1.c

--- day75code1.c
+++ day75code1.c
@@ -1,3 +1,6 @@
+//Open an existing file in append mode and allow the user to enter a new line of text. Append the text at the end without overwriting existing content.
+
+
 #include <stdio.h>
 
 int main() {
