388a025356ef3288f22120a19536b917eb7367a6
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-28 22:49:15 +0530

    Add files via upload

---
+++ day86code1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+enum Operation {
+    ADD = 1,
+    SUBTRACT,
+    MULTIPLY
+};
+
+int main() {
+    int choice;
+    float a, b, result;
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &a, &b);
+
+    printf("\nChoose Operation:\n");
+    printf("1. ADD\n");
+    printf("2. SUBTRACT\n");
+    printf("3. MULTIPLY\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case ADD:
+            result = a + b;
+            printf("Result = %.2f\n", result);
+            break;
+
+        case SUBTRACT:
+            result = a - b;
+            printf("Result = %.2f\n", result);
+            break;
+
+        case MULTIPLY:
+            result = a * b;
+            printf("Result = %.2f\n", result);
+            break;
+
+        default:
+            printf("Invalid choice!\n");
+    }
+
+    return 0;
+}
