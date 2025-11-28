f682f8783fa1d00117247a9caf1f3069cb230099
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-28 22:04:57 +0530

    Update day76code1.c

--- day76code1.c
+++ day76code1.c
@@ -1,4 +1,6 @@
-#include <stdio.h>
+//Ask the user for a filename. Check if it exists by trying to open it in read mode. If the file pointer is NULL, print an error message; otherwise, read and display its content.
+    
+ #include <stdio.h>
 
 int main() {
     FILE *fp;
