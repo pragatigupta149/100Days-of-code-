813ef2cb0376680bffcaeb57570629622444f413
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-28 21:55:36 +0530

    Update day74code1.c

--- day74code1.c
+++ day74code1.c
@@ -1,3 +1,6 @@
+//Take two filenames from the user – a source file and a destination file. Copy all the content from the source file to the destination file using fgetc() and fputc().
+
+
 #include <stdio.h>
 
 int main() {
