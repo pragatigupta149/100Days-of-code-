8cf55b198b8433986fc51fa7ea6bc04b94910cf8
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-28 22:13:23 +0530

    Add files via upload

---
+++ day80code1.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll;
+    float marks;
+};
+
+int main() {
+    FILE *fp;
+    struct Student s;
+    int n, i;
+
+    // Open file in write mode
+    fp = fopen("students.txt", "w");
+    if (fp == NULL) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+
+    printf("Enter number of students: ");
+    scanf("%d", &n);
+
+    // Write student records to file
+    for (i = 0; i < n; i++) {
+        printf("\nEnter details for student %d\n", i + 1);
+        printf("Name: ");
+        scanf("%s", s.name);
+        printf("Roll Number: ");
+        scanf("%d", &s.roll);
+        printf("Marks: ");
+        scanf("%f", &s.marks);
+
+        fprintf(fp, "%s %d %.2f\n", s.name, s.roll, s.marks);
+    }
+
+    fclose(fp);
+    printf("\nStudent records saved successfully to students.txt\n\n");
+
+    // Open file for reading
+    fp = fopen("students.txt", "r");
+    if (fp == NULL) {
+        printf("Error reading file!\n");
+        return 1;
+    }
+
+    printf("Reading student records from file:\n\n");
+    printf("Name\tRoll\tMarks\n");
+    printf("--------------------------------\n");
+
+    // Read and display
+    while (fscanf(fp, "%s %d %f", s.name, &s.roll, &s.marks) != EOF) {
+        printf("%s\t%d\t%.2f\n", s.name, s.roll, s.marks);
+    }
+
+    fclose(fp);
+    return 0;
+}
