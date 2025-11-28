09ed6e49c655b9b4aeecfb921638d828731b36cc
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-28 23:34:58 +0530

    Add files via upload

---
+++ day100code1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+// Structure definition
+struct Student {
+    char name[50];
+    int roll_no;
+    float marks;
+};
+
+int main() {
+    struct Student s;         // Structure variable
+    struct Student *ptr;      // Pointer to structure
+
+    ptr = &s;  // Assign structure address to pointer
+
+    // Modify structure members using -> operator
+    printf("Enter student name: ");
+    scanf("%s", ptr->name);
+
+    printf("Enter roll number: ");
+    scanf("%d", &ptr->roll_no);
+
+    printf("Enter marks: ");
+    scanf("%f", &ptr->marks);
+
+    // Display values using -> operator
+    printf("\n--- Student Details ---\n");
+    printf("Name      : %s\n", ptr->name);
+    printf("Roll No   : %d\n", ptr->roll_no);
+    printf("Marks     : %.2f\n", ptr->marks);
+
+    return 0;
+}
