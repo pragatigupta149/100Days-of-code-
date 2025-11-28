f0c6c12e13148f4ed6a7e41ef4c53478bd451ca2
pragatigupta149 <pragatigupta8037128@gmail.com>
2025-11-28 21:54:46 +0530

    Add files via upload

---
+++ day74code1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    FILE *src, *dest;
+    char sourceFile[50], destFile[50];
+    int ch;
+
+    // Input filenames
+    printf("Enter source file name: ");
+    scanf("%s", sourceFile);
+
+    printf("Enter destination file name: ");
+    scanf("%s", destFile);
+
+    // Open files
+    src = fopen(sourceFile, "r");
+    dest = fopen(destFile, "w");
+
+    if (src == NULL) {
+        printf("Error: Source file not found!\n");
+        return 1;
+    }
+    if (dest == NULL) {
+        printf("Error: Destination file cannot be created!\n");
+        fclose(src);
+        return 1;
+    }
+
+    // Copy content character by character
+    while ((ch = fgetc(src)) != EOF) {
+        fputc(ch, dest);
+    }
+
+    printf("File copied successfully from %s to %s\n", sourceFile, destFile);
+
+    // Close files
+    fclose(src);
+    fclose(dest);
+
+    return 0;
+}
